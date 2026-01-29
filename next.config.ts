1beb4720dfb3781eca9a43980fb74e4128df9d43
Santosh Bastola <santoshbastola@Santoshs-Mac-mini.local>
2026-01-29 13:14:01 +0545

    fix: Enable static export for Firebase deployment

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const withPWA = require("next-pwa")({
 import packageJson from './package.json';
 
 const nextConfig: NextConfig = {
-  // output: 'export',
+  output: 'export',
   images: {
     unoptimized: true,
   },
